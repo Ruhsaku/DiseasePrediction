feb100d06d383b9e0a0522a0e2cd3878a3f5f81b
Stanislav <stanitev@gmail.com>
2025-08-05 12:07:13 +0300

    read and write to json functions

---
+++ file_processing.py
@@ -0,0 +1,26 @@
+import json
+import pandas as pd
+
+def save_to_json(data_series:pd.Series, filename:str):
+    hash_map = {}
+
+    for index, value in enumerate(data_series):
+        hash_map[index] = value
+
+    try:
+        with open(filename, "w") as json_file:
+            json.dump(obj=hash_map, fp=json_file, indent=4)
+    except PermissionError as perm_err:
+        print(f"PermissionError: {perm_err.args}")
+    except IOError as io_err:
+        print(f"IOError: {io_err.args}")
+
+
+def load_from_json(filename:str):
+    try:
+        with open(filename, "r") as json_file:
+            return json.load(fp=json_file)
+    except FileNotFoundError as missing_err:
+        print(f"File not found: {missing_err.args}")
+    except json.JSONDecodeError as decoding_err:
+        print(f"Invalid JSON format: {decoding_err.args}")
